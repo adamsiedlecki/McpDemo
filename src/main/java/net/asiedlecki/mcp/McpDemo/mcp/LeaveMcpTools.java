3bc3c0b283b8b7dd331345538f9dee794fefe3f0
adamsiedlecki <adamsiedlec@gmail.com>
2025-12-16 14:27:15 +0100

    dodanie toola do pobierania listy pracowników

--- src/main/java/net/asiedlecki/mcp/McpDemo/mcp/LeaveMcpTools.java
+++ src/main/java/net/asiedlecki/mcp/McpDemo/mcp/LeaveMcpTools.java
@@ -1,24 +1,53 @@
 package net.asiedlecki.mcp.McpDemo.mcp;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.asiedlecki.mcp.McpDemo.model.Employee;
 import net.asiedlecki.mcp.McpDemo.utils.LeaveService;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class LeaveMcpTools {
 
 
     private final LeaveService leaveService;
+    private final ResourceLoader resourceLoader;
 
+    @Tool(
+            name = "employees",
+            description = """
+                    Zwraca listę nazw pracowników, wraz informacją o tym ile każdemu z nich zostało urlopu do wykorzystania
+                     oraz ile tego urlopu pracownikowi przysługiwało w sumie.
+                    """
+    )
+    public List<Employee> employees() {
+        log.info("Invoked employees");
+        try {
+            String content = resourceLoader
+                    .getResource("classpath:urlopy test.csv")
+                    .getContentAsString(StandardCharsets.UTF_8);
 
-    public LeaveMcpTools(LeaveService leaveService) {
-        this.leaveService = leaveService;
+            return content.lines()
+                    .skip(1) // pomijamy nagłówek
+                    .map(line -> line.split(";"))
+                    .map(parts -> new Employee(
+                            parts[0],
+                            Integer.parseInt(parts[1]),
+                            Integer.parseInt(parts[2])
+                    ))
+                    .toList();
+        } catch (Exception e) {
+            log.error("Error during fetching employees");
+            throw new RuntimeException(e);
+        }
     }
 
 
